66d3f0d8ac6fa1cda2862bd665dd7c97faf9b666
Beatriz aquino <beatrizzoldyck07@gmail.com>
2023-03-02 22:09:26 -0400

    testando os testes

--- Test/index.spec.js
+++ Test/index.spec.js
@@ -1,3 +1,6 @@
+//toBe utiliza para comparar coisas não primitivas
+//toEqual é utilizado para comparar tipos primitivos
+
 import {soma,subtracao,multiplicacao,divisao} from './index.js'
 
 
@@ -7,7 +10,11 @@ describe('Testes de calculadora com valores inteiro ', () => {
         const retornado = soma(10,20);
         expect(retornado).toBe(esperado);
     });
-
+    //criar o teste para retornar dois objetos 
+    /* it('Deve retornar a comparação de dois objetos', () => {
+        const objTestPessoa = {nome: "Beatriz", idade:90}
+        expect(objTest).toEqual({nome: "Beatriz", idade:90});
+    }) */
 
 });
 describe('Testes de calculadora com valores flutuante ', () => {
@@ -28,3 +35,21 @@ describe('Testes de calculadora com valores negativos ', () => {
 
 
 });
+
+describe('Testes de calculadora com multiplicação', () =>{
+    it('Deve retornar a soma de valor inteiro ', () => {
+        const esperado = 15;
+        const retornado = multiplicacao(3, 5);
+        expect(retornado).toBe(esperado);
+    });
+});
+
+
+describe('Teste de calculadora com divisão', () => {
+    it( 'Deve retornar a divisão', () => {
+        const esperado = 6;
+        const retornado = subtracao(12,6) ;
+        expect(retornado).toBe(esperado)
+    });
+});
+
